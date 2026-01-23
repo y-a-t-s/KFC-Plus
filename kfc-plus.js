9e68bd191326b2f106a70083c02aadc94c7a0a21
y a t s <140337963+y-a-t-s@users.noreply.github.com>
2026-01-23 18:04:51 -0500

    More refactoring

--- kfc-plus.js
+++ kfc-plus.js
@@ -4,7 +4,7 @@
 // @description  Chyat, but with +.
 // @downloadURL  https://github.com/y-a-t-s/KFC-Plus/raw/refs/heads/master/kfc-plus.js
 // @homepageURL  https://github.com/y-a-t-s/KFC-Plus
-// @version      0.2.0
+// @version      0.2.1
 // @match        https://kiwifarms.*/test-chat
 // @match        *://kiwifarmsaaf4t2h7gc3dfc5ojhmqruw2nit3uejrpiagrxeuxiyxcyd.onion/test-chat
 // @run-at       document-start
@@ -12,102 +12,160 @@
 
 const green = "#72ff72";
 
-// == Options ==
+// User config
+const config = {
+  autoGreentext: true,
+  fixMemLeak: true,
+  hideUserList: true,
+  scrollInput: true
+};
 
-const autoGreentext = true;
-const fixMemLeak = true;
-const hideUserList = true;
-const scrollInput = true;
+// Text replacement pairs for outgoing messages.
+const replacements = [];
 
-// =============
+const Utils = (() => {
+  // Add style to element.
+  function addStyle(elem, rule) {
+    const style = elem.getAttribute("style") ?? "";
+    if (!rule.endsWith(";"))
+      rule += ";";
 
-var lastSent = "";
+    elem.setAttribute("style", style + rule);
+  }
 
-// Add style to element.
-function addStyle(elem, rule) {
-  const style = elem.getAttribute("style") ?? "";
-  if (!rule.endsWith(";"))
-    rule += ";";
+  // A weird hack to unescape ampersand characters in raw messages.
+  // Currently unused, but will be used soon.
+  var txtArea = document.createElement("textarea");
+  function unEsc(str) {
+    txtArea.innerHTML = str;
+    return txtArea.value;
+  }
 
-  elem.setAttribute("style", style + rule);
-}
+  return {
+    addStyle,
+    unEsc
+  };
+})();
 
 // See here: https://kiwifarms.st/threads/sneedchat-complaint-thread.120982/post-23530505
 function patchLeak() {
-  const t = document.querySelector("template#tmp-chat-user");
-  t.innerHTML = `<div class="activity"><a class="avatar" aria-hidden="true" src="" alt=""></a><a class="user"></a></div>`;
+  const userTemplate = document.querySelector("template#tmp-chat-user");
+  userTemplate.content.querySelector("img.avatar").outerHTML = `<a class="avatar" src="" style="display: none;"></a>`;
 }
 
-// A weird hack to unescape ampersand characters.
-function unEsc(str) {
-  var txt = document.createElement("textarea");
-  txt.innerHTML = str;
-
-  return txt.value;
-}
-
-document.addEventListener("DOMContentLoaded", (e) => {
+const MsgInput = (() => {
   const chatInput = document.querySelector("#new-message-input");
+  if (!chatInput)
+    return null;
 
-  function applyMods(msg) {
-    // msg[0] is undefined if msg is empty.
+  function doReplace() {
+    if (!replacements.length)
+      return msg;
+
+    var msg = getText();
+    if (!msg)
+      return msg;
+
+    replacements.forEach((r) => {
+      msg = msg.replaceAll(r[0], r[1]);
+    });
+
+    setText(msg);
+    return msg;
+  }
+
+  function autoGT() {
+    var msg = getText();
+    if (!msg)
+      return msg;
+
     switch (msg[0]) {
     case "\\":
       msg = msg.replace("\\", "");
       break;
     case ">":
-      if (autoGreentext)
-        msg = `[color=${green}]` + msg;
-
+      msg = `[color=${green}]` + msg;
       break;
     }
 
+    setText(msg);
     return msg;
   }
 
-  function enableInputScroll() {
-    addStyle(chatInput, "overflow-y: auto!important");
+  function enableScroll() {
+    Utils.addStyle(chatInput, "overflow-y: auto!important");
   }
 
-  function hideUsers() {
-    addStyle(document.querySelector("div#chat-activity-scroller"), "display: none;");
+  function getText() {
+    return chatInput.innerText;
   }
 
-  if (fixMemLeak)
+  function setText(msg) {
+    chatInput.innerHTML = msg;
+  }
+
+  return {
+    node: chatInput,
+    autoGT,
+    doReplace,
+    enableScroll,
+    getText,
+    setText
+  };
+});
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  const input = MsgInput();
+  var lastSent = "";
+
+  function hideUserList() {
+    const userPane = document.querySelector("div#chat-activity-scroller");
+    Utils.addStyle(userPane, "display: none;");
+  }
+
+  if (config.fixMemLeak)
     patchLeak();
 
-  if (hideUserList)
-    hideUsers();
+  if (config.hideUserList)
+    hideUserList();
 
-  if (scrollInput)
-    enableInputScroll();
+  if (config.scrollInput)
+    input.enableScroll();
 
-  chatInput.addEventListener("keydown", (e) => {
+  input.node.addEventListener("keydown", (e) => {
     switch (e.key) {
     case "Enter":
       if (e.shiftKey) {
         e.stopImmediatePropagation();
-        chatInput.getRangeAt(0).insertNode(new Text("\n"));
+
+        const sel = window.getSelection();
+        sel.getRangeAt(0).insertNode(new Text("\n"));
 
         return;
       }
 
-      var msg = applyMods(chatInput.innerText);
+      if (config.autoGreentext)
+        input.autoGT();
+
+      const msg = input.getText();
       if (!msg)
         return;
 
-      chatInput.innerHTML = msg;
       lastSent = msg;
 
       break;
     case "Tab":
       // Stop focus from shifting away.
       e.preventDefault();
+      input.doReplace();
+
+      break;
     case "ArrowDown":
-      switch (chatInput.innerText) {
+      var contents = input.getText();
+      switch (contents) {
       case "":
       case "\n": // Sometimes an invisible <br> appears in the input field when empty. idk why.
-        chatInput.innerHTML = lastSent;
+        input.setText(lastSent);
       }
 
       break;
